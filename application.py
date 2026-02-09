f351b8569014e1f0643689d194216543c496b15e
Taha Tariq <tahatariqf@gmail.com>
2026-02-09 03:25:47 -0800

    Done

--- application.py
+++ application.py
@@ -40,5 +40,5 @@ def form():
     else:
         return render_template("form.html")
     
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0")
+# if __name__ == "__main__":
+#     app.run(host = "0.0.0.0")
